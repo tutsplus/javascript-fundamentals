0e5e31f47a3f1e6702653f686cbb98f390afbbc9
Dan Wellman <danwellman@hotmail.com>
2016-10-22 17:10:41 +0100

    Update arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -1,7 +1,8 @@
-﻿(function () {
+(function () {
     'use strict';
 
     var array = ['one', 'two', 'three'];
+
     console.log(array.length); // 3
 
     array.push('four');
@@ -10,33 +11,59 @@
     array.unshift('zero');
     console.log(array); // ['zero', 'one', 'two', 'three', 'four']
 
-    console.log(array.pop()); // four
-    console.log(array.shift()); // zero
+    array.pop();
+    console.log(array); // ['zero', 'one', 'two', 'three']
+
+    array.shift();
+    console.log(array); // ['one', 'two', 'three']
 
     array.splice(0, 3, 'a', 'b', 'c');
     console.log(array); // ['a', 'b', 'c']
 
-    console.log(array.reverse()); // ['c', 'b', 'a']
+    array.reverse();
+    console.log(array); // ['c', 'b', 'a']
 
-    console.log(array.sort()); // ['a', 'b', 'c']
+    array.sort();
+    console.log(array); // ['a', 'b', 'c']
 
-    console.log(['a', 'b', 'C'].sort()); // ['C', 'a', 'b']
-    console.log([3, 20, 10000].sort(function (a, b) {
-        if (a < b) {
+    console.log(['a', 'C', 'b'].sort()); // ['C', 'a', 'b']
+
+    console.log([20, 10000, 3].sort()); // [10000, 20, 3]
+
+    console.log([20, 10000, 3].sort(function (a, b) {
+        return a - b;
+    })); // [3, 20, 10000]
+
+    console.log(['a', 'C', 'b'].sort(function (a, b) {
+        var caselessA = a.toLowerCase(),
+            caselessB = b.toLowerCase();
+
+        if (caselessA < caselessB) {
             return -1;
-        } else if (a === b) {
+        } else if (caselessA === caselessB) {
             return 0;
         } else {
             return 1;
         }
-    })); // [3, 20, 10000]
+    })); // ['a', 'b', 'C']
+
+    console.log([{ name: 'a' }, { name: 'C' }, { name: 'b' }].sort(function (a, b) {
+        var caselessA = a.name.toLowerCase(),
+            caselessB = b.name.toLowerCase();
+
+        if (caselessA < caselessB) {
+            return -1;
+        } else if (caselessA === caselessB) {
+            return 0;
+        } else {
+            return 1;
+        }
+    })); // [{ name: 'a' }, { name: 'b' }, { name: 'C' }]
 
     console.log(array.join('')); // abc
 
     console.log(array.indexOf('b')); // 1
 
-    console.log(array.slice(1)); // ['b', 'c']
-    console.log(array.slice(-2)); // ['b', 'c']
     console.log(array.slice(0, -1)); // ['a', 'b']
 
     array.forEach(function (value, index) {
